0b0b6839b22ecb04fd1e8c205048f9be094e62ca
kwb020312 <kwb020312@naver.com>
2021-07-19 09:59:56 +0900

    Section 2 - 1

--- README.md
+++ README.md
@@ -5,9 +5,3 @@
 해당 저장소는 인프런 <a href="https://www.inflearn.com/course/node-js-%EC%9B%B9%EA%B0%9C%EB%B0%9C/dashboard">node-js-웹개발</a> 을 보고 참고하여 만들어졌음을 밝힙니다.
 
 <img src="gitImages\Main_Title.PNG">
-
-## 간단한 서버 구축
-
-<img src="gitImages\Create_Main_Page.PNG">
-
-메인 페이지를 구축해보고 사진을 첨부함
--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/main.html");
+});
+
+app.get("/main", (req, res) => {
+  res.sendFile(__dirname + "/public/main.html");
+});
+
+app.post("/email_post", (req, res) => {
+  res.send(`<h1>Welcome, ${req.body.email}</h1>`);
 });
 
 app.listen(3000, () => {
