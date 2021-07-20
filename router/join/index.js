6b20bff9f34725d29986a289afc342ed77058da5
kwb020312 <kwb020312@naver.com>
2021-07-20 14:27:17 +0900

    Section 5. Add Data to DB - 2

--- router/join/index.js
+++ router/join/index.js
@@ -17,4 +17,17 @@ router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../public/join.html"));
 });
 
+router.post("/", (req, res) => {
+  const body = req.body;
+  const { email, name, password } = body;
+
+  const query = connection.query(
+    `insert into user (email,name,pw) values("${email}", "${name}","${password}");`,
+    (err, rows) => {
+      if (err) throw err;
+      console.log("ok db insert");
+    }
+  );
+});
+
 module.exports = router;
