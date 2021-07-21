6f09ac638c83fe4f7afd9c0f98349e24d3fb550e
kwb020312 <kwb020312@naver.com>
2021-07-21 15:59:19 +0900

    Section 6. Logout Process

--- router/logout/index.js
+++ router/logout/index.js
@@ -2,10 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  console.log("main js loaded");
-  const id = req.user;
-  if (!id) res.render("login.ejs");
-  res.render("main.ejs", { id: id });
+  req.logOut();
+  res.redirect("/login");
 });
 
 module.exports = router;
